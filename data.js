3db056f6fe2ee90e3718054b098a085535fc3548
iris151515 <iris52089@gmail.com>
2026-05-27 17:06:20 +0800

    card,modal連結成功、篩選標籤初版

---
+++ data.js
@@ -0,0 +1,49 @@
+// 所有咖啡廳
+var cafeList = [
+    { 
+        num: 1, 
+        pic: "fakecafe.jpg", 
+        name: "南青山咖啡", 
+        highlight: "和貓咪共度悠閒午後", 
+        tag: ["#不限時", "#輕食"], 
+        dist: "南區", 
+        map: "https://maps.app.goo.gl/Xir8xgHPArYdqdKg9" 
+    },
+    { 
+        num: 2, 
+        pic: "fakecafe2.jpg", 
+        name: "悅癮咖啡 Andorfin Coffee", 
+        highlight: "咖啡與美食，愉悅成癮的日常", 
+        tag: ["#早晨營業", "#精品咖啡", "#不限時"], 
+        dist: "南屯區", 
+        map: "https://maps.app.goo.gl/Xir8xgHPArYdqdKg9" 
+    },
+    { 
+        num: 3, 
+        pic: "fakecafe.jpg", 
+        name: "3號咖啡", 
+        highlight: "3號簡介", 
+        tag: ["標籤"], 
+        dist: "3號地區", 
+        map: "https://maps.app.goo.gl/Xir8xgHPArYdqdKg9" 
+    },
+    { 
+        num: 4, 
+        pic: "fakecafe.jpg", 
+        name: "4號咖啡",
+        highlight: "4號簡介", 
+        tag: ["標籤"], 
+        dist: "4號地區",
+        map: "https://maps.app.goo.gl/Xir8xgHPArYdqdKg9" 
+    },
+    { 
+        num: 5, 
+        pic: "fakecafe2.jpg", 
+        name: "5號咖啡", 
+        highlight: "5號簡介", 
+        tag: ["標籤"], 
+        dist: "5號地區", 
+        map: "https://maps.app.goo.gl/Xir8xgHPArYdqdKg9" 
+    },
+    
+];
